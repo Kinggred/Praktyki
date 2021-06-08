80376c72db799118bbc000d9f4e6af2565878b96
Kinggred <maks.grupinski@gmail.com>
2021-06-08 13:45:34 +0000

    0.012

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,6 +1,8 @@
 import json as js
 import requests as rq
 
-r = rq.get('https://raw.githubusercontent.com/Kinggred/Praktyki/quiz/quiz/text.txt')
+r = rq.get('https://raw.githubusercontent.com/Kinggred/Praktyki/quiz/quiz/text.json')
+quest = js.loads(r.content)
 
-print(r.content)
+
+print()
